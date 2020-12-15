9e701b3e3a1e36caf91d8232b61260f35652a3d0
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-12-15 10:43:41 -0500

    References

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,25 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Maybe seed data with external API
+List.destroy_all
+Item.destroy_all
 
-List.create(name: 'Vegetables')
+veg = List.create(name: 'Vegetables')
+fruit = List.create(name: 'Fruit')
+past = List.create(name: 'Pastries')
+milk = List.create(name: 'Milk')
+live = List.create(name: 'Livestock')
+crops = List.create(name: 'Crops')
+nuts = List.create(name: 'Nuts')
+berries = List.create(name: 'Berries')
 
-Item.create(content: 'Carrots', list: List.all.sample)
-Item.create(content: 'Squash', list: List.all.sample)
-Item.create(content: 'Cucumber', list: List.all.sample)
-Item.create(content: 'Brocolli', list: List.all.sample)
+herb = Item.create(content: 'Fresh Rosemary', list_id: crops.id)
+app = Item.create(content: 'Home-baked Apple Pies', list_id: past.id)
+sqh = Item.create(content: 'Organic Squash', list_id: veg.id)
+cucu = Item.create(content: 'Homegrown Cucumbers', list_id: veg.id)
+kw = Item.create(content: 'Organic Kiwi', list_id: fruit.id)
+ban = Item.create(content: 'Banana', list_id: fruit.id)
+sp = Item.create(content: 'Farm-raised Sheep', list_id: live.id)
+alm = Item.create(content: 'Almond Milk', list_id: milk.id)
+pea = Item.create(content: 'Organic Peanuts', list_id: nuts.id)
+cran = Item.create(content: 'Organic Cranberries', list_id: berries.id)
