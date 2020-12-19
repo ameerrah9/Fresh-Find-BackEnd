82cab535341ee3f9d0580e29b712bb76366e67d2
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-12-19 13:28:13 -0500

    Database changed

--- db/migrate/20201205223415_create_lists.rb
+++ db/migrate/20201205223415_create_lists.rb
@@ -2,7 +2,7 @@ class CreateLists < ActiveRecord::Migration[6.0]
   def change
     create_table :lists do |t|
       t.string :name
-
+      t.references :item, null: false, foreign_key: true
       t.timestamps
     end
   end
